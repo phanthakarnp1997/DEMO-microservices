e827ce08021e8dcd7cf33b8c22d890d60daa6520
dew_phanthakarn <phanthakarn.plukpanya@gmail.com>
2023-08-07 20:01:14 +0700

    config load-balancer

--- currency-convension-service/src/main/java/com/demo/microservice/currencyconvensionservice/proxy/CurrencyExchangeProxy.java
+++ currency-convension-service/src/main/java/com/demo/microservice/currencyconvensionservice/proxy/CurrencyExchangeProxy.java
@@ -1,11 +1,13 @@
 package com.demo.microservice.currencyconvensionservice.proxy;
 
 import com.demo.microservice.currencyconvensionservice.model.CurrencyConvension;
+import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "currency-exchange", url = "localhost:8000")
+//@FeignClient(name = "currency-exchange", url = "localhost:8000")
+@FeignClient(name = "currency-exchange-service")
 public interface CurrencyExchangeProxy {
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     public CurrencyConvension getExchangeValue(@PathVariable String from, @PathVariable String to);
