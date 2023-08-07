59457861c8d18f18b370f2aa241208a7c813a0c5
keang1996 <omyim189@hotmail.com>
2023-08-07 23:36:08 +0700

    limit_test: add ITCase

--- limits-service/src/test/java/com/demo/microservice/limitsservice/ITCase/LimitITCase.java
+++ limits-service/src/test/java/com/demo/microservice/limitsservice/ITCase/LimitITCase.java
@@ -1,6 +1,5 @@
 package com.demo.microservice.limitsservice.ITCase;
 
-import com.demo.microservice.limitsservice.model.Limits;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
