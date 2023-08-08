04ebbf339e55d960e83753e38be501a9da8db391
dew_phanthakarn <phanthakarn.plukpanya@gmail.com>
2023-08-08 19:31:34 +0700

    Implement gateway-api, log filtering

--- api-gateway/src/main/java/com/demo/microservice/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/demo/microservice/apigateway/ApiGatewayApplication.java
@@ -2,12 +2,14 @@ package com.demo.microservice.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ApiGatewayApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ApiGatewayApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
 }
--- currency-convension-service/src/main/java/com/demo/microservice/currencyconvensionservice/controller/CurrencyConvensionController.java
+++ currency-convension-service/src/main/java/com/demo/microservice/currencyconvensionservice/controller/CurrencyConvensionController.java
@@ -4,6 +4,8 @@ import com.demo.microservice.currencyconvensionservice.model.CurrencyConvension;
 import com.demo.microservice.currencyconvensionservice.proxy.CurrencyExchangeProxy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,17 +22,19 @@ public class CurrencyConvensionController {
     private Environment environment;
 
     @GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
-    public CurrencyConvension getCurrencyConvension(@PathVariable String from, @PathVariable String to, @PathVariable BigDecimal quantity) {
+    public ResponseEntity<CurrencyConvension> getCurrencyConvension(@PathVariable String from, @PathVariable String to, @PathVariable BigDecimal quantity) {
 
         CurrencyConvension currencyConvension = proxy.getExchangeValue(from, to);
 
-        return new CurrencyConvension(currencyConvension.getId(),
+        CurrencyConvension result = new CurrencyConvension(currencyConvension.getId(),
                 currencyConvension.getFrom(),
                 currencyConvension.getTo(),
                 currencyConvension.getConversionMultiple(),
                 quantity,
                 quantity.multiply(currencyConvension.getConversionMultiple()),
                 currencyConvension.getEnvironment());
+
+        return new ResponseEntity<CurrencyConvension>(result, HttpStatus.OK);
     }
 
 }
