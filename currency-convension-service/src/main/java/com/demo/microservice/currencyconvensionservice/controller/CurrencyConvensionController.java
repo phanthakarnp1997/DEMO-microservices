6bf48e916b6dfd55aeed93aa33268476f2e6439d
dew_phanthakarn <55444569+phanthakarnp1997@users.noreply.github.com>
2023-08-10 21:26:33 +0700

    implemented Circuit Breaker (#5)

--- currency-convension-service/src/main/java/com/demo/microservice/currencyconvensionservice/controller/CurrencyConvensionController.java
+++ currency-convension-service/src/main/java/com/demo/microservice/currencyconvensionservice/controller/CurrencyConvensionController.java
@@ -2,6 +2,8 @@ package com.demo.microservice.currencyconvensionservice.controller;
 
 import com.demo.microservice.currencyconvensionservice.model.CurrencyConvension;
 import com.demo.microservice.currencyconvensionservice.proxy.CurrencyExchangeProxy;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
@@ -18,10 +20,9 @@ public class CurrencyConvensionController {
     @Autowired
     private CurrencyExchangeProxy proxy;
 
-    @Autowired
-    private Environment environment;
-
     @GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
+//    @RateLimiter(name = "currency-conversion-rateLimiter")
+    @CircuitBreaker(name = "currency-conversion-service", fallbackMethod = "getDefaultExchange")
     public ResponseEntity<CurrencyConvension> getCurrencyConvension(@PathVariable String from, @PathVariable String to, @PathVariable BigDecimal quantity) {
 
         CurrencyConvension currencyConvension = proxy.getExchangeValue(from, to);
@@ -34,7 +35,11 @@ public class CurrencyConvensionController {
                 quantity.multiply(currencyConvension.getConversionMultiple()),
                 currencyConvension.getEnvironment());
 
-        return new ResponseEntity<CurrencyConvension>(result, HttpStatus.OK);
+        return new ResponseEntity(result, HttpStatus.OK);
+    }
+
+    public ResponseEntity<CurrencyConvension> getDefaultExchange(Exception e) {
+        return new ResponseEntity(new CurrencyConvension(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
 }
