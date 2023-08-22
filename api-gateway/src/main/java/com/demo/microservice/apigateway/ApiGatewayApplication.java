22a56bc57e1c3a45eef7c841107993fd46886b65
dew_phanthakarn <phanthakarn.plukpanya@gmail.com>
2023-08-22 22:51:30 +0700

    feat: config swagger routes dynamic
    feat: add swagger to project
    
    feat: add dynamic swagger routes

--- api-gateway/src/main/java/com/demo/microservice/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/demo/microservice/apigateway/ApiGatewayApplication.java
@@ -1,14 +1,15 @@
 package com.demo.microservice.apigateway;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
-import org.springframework.context.annotation.Bean;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@OpenAPIDefinition(info = @Info(title = "api-gateway-doc", version = "1.0", description = "api-gateway-doc"))
 public class ApiGatewayApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
     }
--- api-gateway/src/main/java/com/demo/microservice/apigateway/configuration/GatewayConfiguration.java
+++ api-gateway/src/main/java/com/demo/microservice/apigateway/configuration/GatewayConfiguration.java
@@ -1,5 +1,7 @@
 package com.demo.microservice.apigateway.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -7,16 +9,32 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class GatewayConfiguration {
+    @Value("${server.port}")
+    private String port;
+
+    @Autowired
+    private SwaggerUIProperties swaggerUIProperties;
+
     @Bean
     public RouteLocator routeLocator(RouteLocatorBuilder builder) {
-        return builder.routes()
-                .route(p -> p
-                        .path("/get")
-//                        .filters(f -> f.addRequestHeader("", ""))
-                        .uri("http://httpbin.org:80"))
-                .route(p -> p
-                        .path("/portfolio/**")
-                        .uri("lb://portfolio-service"))
-                .build();
+        RouteLocatorBuilder.Builder routes = builder.routes();
+
+        routes.route("portfolio-route", p -> p
+                .path("/portfolio/**")
+                .uri("lb://portfolio-service"));
+
+        routes.route("openapi-route", p -> p
+                .path("/v3/api-docs/**")
+                .filters(f -> f.rewritePath("/v3/api-docs/(?<path>.*)", "/${path}/v3/api-docs"))
+                .uri("http://localhost:" + port));
+
+        // Add dynamically swagger configured routes
+        for (SwaggerUIProperties.UrlConfiguration urlConfiguration : swaggerUIProperties.getUrls()) {
+            routes.route(urlConfiguration.getName(), p -> p
+                    .path(urlConfiguration.getUrl())
+                    .uri("lb://" + urlConfiguration.getName()));
+        }
+
+        return routes.build();
     }
 }
