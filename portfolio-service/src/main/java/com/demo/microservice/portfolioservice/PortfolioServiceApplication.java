c88ffe741c830edc7a71ab1dc14f4ffa1fcbf70f
dew_phanthakarn <55444569+phanthakarnp1997@users.noreply.github.com>
2023-08-23 20:29:03 +0700

    feat: config swagger routes dynamic (#14)
    * feat(User): CRUD
    
    * refactor: optimize import unused
    
    * feat: config swagger routes dynamic
    
    feat: add swagger to project
    
    feat: add dynamic swagger routes
    
    * refactor: optimize import unused
    
    * feat: config swagger routes dynamic
    
    feat: add swagger to project
    
    feat: add dynamic swagger routes
    
    * feat(User): CRUD
    
    * refactor: optimize import unused
    
    * feat: config swagger routes dynamic
    
    feat: add swagger to project
    
    feat: add dynamic swagger routes

--- portfolio-service/src/main/java/com/demo/microservice/portfolioservice/PortfolioServiceApplication.java
+++ portfolio-service/src/main/java/com/demo/microservice/portfolioservice/PortfolioServiceApplication.java
@@ -1,13 +1,16 @@
 package com.demo.microservice.portfolioservice;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "portfolio-api-doc", version = "1.0", description = "portfolio-api-doc"))
 public class PortfolioServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PortfolioServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PortfolioServiceApplication.class, args);
+    }
 
 }
