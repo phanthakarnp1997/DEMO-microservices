723d280cd90d197f3330eb61c062f0ef19f3f2a6
dew_phanthakarn <phanthakarn.plukpanya@gmail.com>
2023-08-26 22:35:48 +0700

    refactor: convert ErrorCodes class to enum

--- common-library/src/main/java/com/example/microservice/commonlibrary/exception/NotFoundException.java
+++ common-library/src/main/java/com/example/microservice/commonlibrary/exception/NotFoundException.java
@@ -1,13 +1,14 @@
 package com.example.microservice.commonlibrary.exception;
 
 import com.example.microservice.commonlibrary.payload.ApiResponse;
+import com.example.microservice.commonlibrary.util.constant.ErrorCodes;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class NotFoundException extends BaseException {
 
-    public NotFoundException(int errorCode, String message) {
+    public NotFoundException(ErrorCodes errorCode, String message) {
         super(errorCode, message);
     }
 
--- common-library/src/main/java/com/example/microservice/commonlibrary/util/constant/ErrorCodes.java
+++ common-library/src/main/java/com/example/microservice/commonlibrary/util/constant/ErrorCodes.java
@@ -1,8 +1,19 @@
 package com.example.microservice.commonlibrary.util.constant;
 
-public class ErrorCodes {
-    public static final int ERROR_BUSINESS_CODE_1 = 999;
-    public static final int BAD_REQUEST_BUSINESS_CODE_1 = 888;
-    public static final int NOT_FOUND_BUSINESS_CODE_1 = 777;
-    public static final int NOT_FOUND_BUSINESS_CODE_2 = 5981;
+import lombok.Getter;
+
+@Getter
+public enum ErrorCodes {
+    DEFAULT_CODE("PFL101"),
+    ERROR_BUSINESS_CODE_1("PFL999"),
+    BAD_REQUEST_BUSINESS_CODE_1("PFL888"),
+    NOT_FOUND_BUSINESS_CODE_1("PFL777"),
+    NOT_FOUND_BUSINESS_CODE_2("PFL5981");
+
+    private final String value;
+
+    ErrorCodes(String value) {
+        this.value = value;
+    }
+
 }
