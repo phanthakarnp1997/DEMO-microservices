e7ee0b9d9fa54504efb4a736dbb54730ad87ae9a
dew_phanthakarn <phanthakarn.plukpanya@gmail.com>
2023-08-30 20:05:13 +0700

    refactor: return response with common response

--- common-library/src/main/java/com/example/microservice/commonlibrary/payload/ApiResponse.java
+++ common-library/src/main/java/com/example/microservice/commonlibrary/payload/ApiResponse.java
@@ -1,8 +1,12 @@
 package com.example.microservice.commonlibrary.payload;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ApiResponse<T> {
     private String status;
     private String message;
