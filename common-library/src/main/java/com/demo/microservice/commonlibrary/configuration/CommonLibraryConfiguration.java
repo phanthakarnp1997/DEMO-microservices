7b38187900b888f8ced150bbb11678c21730a85e
dew_phanthakarn <phanthakarn.plukpanya@gmail.com>
2023-09-02 18:06:39 +0700

    refactor: Move EnableJpaRepositories, EntityScan, ComponentScan to common-library

--- common-library/src/main/java/com/demo/microservice/commonlibrary/configuration/CommonLibraryConfiguration.java
+++ common-library/src/main/java/com/demo/microservice/commonlibrary/configuration/CommonLibraryConfiguration.java
@@ -1,7 +1,13 @@
 package com.demo.microservice.commonlibrary.configuration;
 
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
+@EnableJpaRepositories(basePackages = {"com.demo.*"})
+@EntityScan(basePackages = {"com.demo.*"})
+@ComponentScan(basePackages = {"com.demo.*"})
 public class CommonLibraryConfiguration {
 }
--- portfolio-service/src/main/java/com/demo/microservice/portfolioservice/PortfolioServiceApplication.java
+++ portfolio-service/src/main/java/com/demo/microservice/portfolioservice/PortfolioServiceApplication.java
@@ -1,17 +1,14 @@
 package com.demo.microservice.portfolioservice;
 
+import com.demo.microservice.commonlibrary.configuration.CommonLibraryConfiguration;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
-@EnableJpaRepositories(basePackages = {"com.demo.microservice.commonlibrary.repository", "com.demo.microservice.portfolioservice.repository"})
-@EntityScan(basePackages = {"com.demo.microservice.commonlibrary", "com.demo.microservice.portfolioservice"})
-@ComponentScan(basePackages = {"com.demo.microservice.commonlibrary", "com.demo.microservice.portfolioservice"})
+@Import(CommonLibraryConfiguration.class)
 @OpenAPIDefinition(info = @Info(title = "portfolio-api-doc", version = "1.0", description = "portfolio-api-doc"))
 public class PortfolioServiceApplication {
 
